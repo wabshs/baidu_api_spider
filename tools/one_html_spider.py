22d885d956fd73f1ee7fdce84983c0c70b57d6c1
Nino <840654613@qq.com>
2023-09-07 11:39:24 +0800

    request_parameters板块爬取完成

--- tools/one_html_spider.py
+++ tools/one_html_spider.py
@@ -29,18 +29,59 @@ def get_html(url: str):
     base_url = code.find_next().find_next().text
     print(base_url)
 
-    # 构建json
-    api_list = {}
-    data = json.loads(json.dumps(api_list))
+    # 先寻找第一个table 然后下下个兄弟节点是我需要的
+    table = html_soup.find(
+        'table')
+    # print(table)
 
-    data['api_name'] = api_name
-    data['api_description'] = api_description
-    data['method'] = method
-    data['base_url'] = base_url
+    # url参数
+    url_param_p = table.find('td')
+    url_param = url_param_p.text
+    print(url_param)
 
-    api_list_json = json.dumps(data,ensure_ascii=False)
+    # header的table
+    header_table = table.find_next_sibling().find_next()
+    header = header_table.find('td').text
+    print(header)
 
-    print(api_list_json)
+    # 开始构建参数列表
+    request_parameters = []  # 里面由几个json构成
+    params_table = table.find_next_sibling().find_next_sibling().find_next_sibling().find_next_sibling().find_next_sibling()
+    # print(params_table)
+
+    # 里面的tbody
+    t_body = params_table.find('tbody')
+    # print(t_body)
+
+    tr_list = t_body.find_all('tr')
+    # print(tr_list)
+
+    # 将tr节点五个一组封装为字典
+    result = [{f: td.text.strip() for f, td in
+               zip(['param_name', 'is_requirment', 'type', 'optional_range', 'description'], tr.find_all('td'))} for tr
+              in tr_list]
+
+    # 化成json
+    result_json = json.dumps(result,ensure_ascii=False)
+
+    request_parameters = result_json
+
+    print(result_json)
+
+    # print(result)
+
+    # # 构建json
+    # api_list = {}
+    # data = json.loads(json.dumps(api_list))
+    #
+    # data['api_name'] = api_name
+    # data['api_description'] = api_description
+    # data['method'] = method
+    # data['base_url'] = base_url
+    #
+    # api_list_json = json.dumps(data,ensure_ascii=False)
+    #
+    # print(api_list_json)
 
 
 if __name__ == '__main__':
